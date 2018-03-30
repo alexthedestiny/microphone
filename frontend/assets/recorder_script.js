1f80e4592066f201e8ce197cdb80d25e6a672f5c
Unknown <oleksandrpavlenko@yahoo.com>
2018-03-30 14:59:55 +0300

    Auto stash before merge of "development" and "origin/development"

--- frontend/assets/recorder_script.js
+++ frontend/assets/recorder_script.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function(){
-	$('.inbenta-bot__launcher').click(function(){
+	jQuery('.inbenta-bot__launcher').click(function(){
+		console.log('click');
 		jQuery('<button onclick="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
     	jQuery('<button onclick="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
 	});
