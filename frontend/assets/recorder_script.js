e28661cda406f89abe4f4496269308ffecfaf97d
Unknown <oleksandrpavlenko@yahoo.com>
2018-03-30 17:19:15 +0300

    trigger 1.4

--- frontend/assets/recorder_script.js
+++ frontend/assets/recorder_script.js
@@ -3,6 +3,6 @@ jQuery(document).ready(function(){
 		setTimeout(()=>{
 			jQuery('<button onclick="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
     		jQuery('<button onclick="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
-		},2000);
+		},500);
 	} );
 });
