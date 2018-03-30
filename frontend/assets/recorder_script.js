3608b3a8c4aa2f47c4973bab3e119deb89ec0c99
Unknown <oleksandrpavlenko@yahoo.com>
2018-03-30 15:06:31 +0300

    add mic 2

--- frontend/assets/recorder_script.js
+++ frontend/assets/recorder_script.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
-	jQuery('.inbenta-bot__launcher').on('click', function(){
+	$(document).on('click', '.inbenta-bot__launcher', ()=>{
 		console.log('click');
 		jQuery('<button onclick="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
     	jQuery('<button onclick="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
-	});
+	} );
   // setTimeout(function(){
   //   jQuery('<button onclick="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
   //   jQuery('<button onclick="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
