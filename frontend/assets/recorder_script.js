0886e5ab4e907785d96ea55c8c438725f2536ae8
Unknown <oleksandrpavlenko@yahoo.com>
2018-03-30 15:10:49 +0300

    add mic 2.2

--- frontend/assets/recorder_script.js
+++ frontend/assets/recorder_script.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function(){
 		setTimeout(()=>{
 			jQuery('<button onclick="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
     		jQuery('<button onclick="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></button>').insertBefore( ".inbenta-bot-button" );
+    		console.log('hmm');
 		},2000);
 	} );
 });
