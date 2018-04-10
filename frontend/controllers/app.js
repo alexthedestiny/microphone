5bc40a4ac4b95c2897a16daf938009f1ec600e87
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-10 19:09:00 +0300

    show mic

--- frontend/controllers/app.js
+++ frontend/controllers/app.js
@@ -114,17 +114,26 @@ InbentaChatbotSDK.build(InbentaAuth, {
         },
         success: function(data) {
           window.InbentaSessionToken = data.sessionToken;
+          showMic();
         }
       });
     }
   });
   chatbot.actions.showConversationWindow();
-  jQuery('<div ng-click="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
-  jQuery('<div ng-click="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
-  window.neededToShow = true;
-  document.getElementById('microphone-button').addEventListener('click', function() {
-    window.startRecording();
-  });
+  // jQuery('<div ng-click="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
+  // jQuery('<div ng-click="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
+  // window.neededToShow = true;
+  // document.getElementById('microphone-button').addEventListener('click', function() {
+  //   window.startRecording();
+  // });
+  function showMic(){
+    jQuery('<div ng-click="startRecording();" style="cursor: pointer; border: none; background: #fff" id="microphone-button" class="microphone-button"><i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
+    jQuery('<div ng-click="stopRecording();" style="display: none; cursor: pointer; border: none; background: #fff" class="microphone-button-slash" id="microphone-button-slash"><i class="fa fa-microphone-slash" style="color: #6ac1ca; font-size: 18px;"></i></div>').insertBefore( ".inbenta-bot-button" );
+    window.neededToShow = true;
+    document.getElementById('microphone-button').addEventListener('click', function() {
+      window.startRecording();
+    });
+  }
   $("#inbenta-bot-input").focus(function(){
     setTimeout(function(){
       console.log($("#inbenta-bot-input").val());
