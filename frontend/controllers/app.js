18d6d4d7fc1d76e2277e3d6428e395a59573d579
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-10 19:39:10 +0300

    dc

--- frontend/controllers/app.js
+++ frontend/controllers/app.js
@@ -135,32 +135,32 @@ InbentaChatbotSDK.build(InbentaAuth, {
     });
   }
   $("#inbenta-bot-input").focus(function(){
-    setTimeout(function(){
+    // setTimeout(function(){
       console.log($("#inbenta-bot-input").val());
-      if($("#inbenta-bot-input").val() && !$("#inbenta-bot-input").attr('data-value')) {
-        $("#inbenta-bot-input").attr('data-value', $("#inbenta-bot-input").val());
-        return;
-      }
-      if($("#inbenta-bot-input").attr('data-value') !== null){
-        if(!$("#inbenta-bot-input").val()) {
-          $("#inbenta-bot-input").val($("#inbenta-bot-input").attr('data-value'));
-        }
-      }
-    },0);
+    //   if($("#inbenta-bot-input").val() && !$("#inbenta-bot-input").attr('data-value')) {
+    //     $("#inbenta-bot-input").attr('data-value', $("#inbenta-bot-input").val());
+    //     return;
+    //   }
+    //   if($("#inbenta-bot-input").attr('data-value') !== null){
+    //     if(!$("#inbenta-bot-input").val()) {
+    //       $("#inbenta-bot-input").val($("#inbenta-bot-input").attr('data-value'));
+    //     }
+    //   }
+    // },0);
   });
   $("#inbenta-bot-input").blur(function(){
-    setTimeout(function(){
+    // setTimeout(function(){
       console.log($("#inbenta-bot-input").val());
-      if($("#inbenta-bot-input").val() && !$("#inbenta-bot-input").attr('data-value')) {
-        $("#inbenta-bot-input").attr('data-value', $("#inbenta-bot-input").val());
-        return;
-      }
-      if($("#inbenta-bot-input").attr('data-value') !== null){
-        if(!$("#inbenta-bot-input").val()) {
-          $("#inbenta-bot-input").val($("#inbenta-bot-input").attr('data-value'));
-        }
-      }
-    },0);
+    //   if($("#inbenta-bot-input").val() && !$("#inbenta-bot-input").attr('data-value')) {
+    //     $("#inbenta-bot-input").attr('data-value', $("#inbenta-bot-input").val());
+    //     return;
+    //   }
+    //   if($("#inbenta-bot-input").attr('data-value') !== null){
+    //     if(!$("#inbenta-bot-input").val()) {
+    //       $("#inbenta-bot-input").val($("#inbenta-bot-input").attr('data-value'));
+    //     }
+    //   }
+    // },0);
   });
   $(document).on('click', '.inbenta-bot-button', function(){
     window.clickNaKnopku = $("#inbenta-bot-input").attr('data-value');
@@ -408,10 +408,9 @@ recorderApp.controller('RecorderController', [ '$scope' , function($scope) {
         console.log("AJAXSubmit - Success!"); //DEBUG
         try {
           result = JSON.parse(result);
-          jQuery("#inbenta-bot-input").val(result.results[0].alternatives[0].transcript);
-          jQuery("#inbenta-bot-input").attr('value', result.results[0].alternatives[0].transcript);
+          // jQuery("#inbenta-bot-input").val(result.results[0].alternatives[0].transcript);
+          // jQuery("#inbenta-bot-input").attr('value', result.results[0].alternatives[0].transcript);
           jQuery("#inbenta-bot-input").attr('data-value', result.results[0].alternatives[0].transcript);
-          jQuery("#inbenta-bot-input").trigger('keydown').trigger('keyup').trigger('keypress');
           jQuery('#microphone-button').html('<i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i>');
         } catch (exc) {
           console.warn('Could not parse result into JSON object: "' + result + '"');
