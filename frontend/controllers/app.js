935689eb701e4ec7d5c7f522f15f5dd0702da80e
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-10 19:53:25 +0300

    va

--- frontend/controllers/app.js
+++ frontend/controllers/app.js
@@ -412,8 +412,8 @@ recorderApp.controller('RecorderController', [ '$scope' , function($scope) {
         console.log("AJAXSubmit - Success!"); //DEBUG
         try {
           result = JSON.parse(result);
-          // jQuery("#inbenta-bot-input").val(result.results[0].alternatives[0].transcript);
-          jQuery("#inbenta-bot-input").attr('value', result.results[0].alternatives[0].transcript);
+          jQuery("#inbenta-bot-input").val(result.results[0].alternatives[0].transcript);
+          // jQuery("#inbenta-bot-input").attr('value', result.results[0].alternatives[0].transcript);
           jQuery("#inbenta-bot-input").attr('data-value', result.results[0].alternatives[0].transcript);
           jQuery('#microphone-button').html('<i class="fa fa-microphone" style="color: #6ac1ca; font-size: 18px;"></i>');
         } catch (exc) {
