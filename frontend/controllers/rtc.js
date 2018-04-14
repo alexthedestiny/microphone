682d1383252a12bf56a6f6bae8e76c97811eb383
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-14 17:51:58 +0300

    custom v2

--- frontend/controllers/rtc.js
+++ frontend/controllers/rtc.js
@@ -43,8 +43,22 @@ if(isSafari) {
 $(function(){
   setTimeout(function(){
     //start custom input
-    $('#inbenta-bot-input, .inbenta-bot-button').css('display','none');
+    $('#inbenta-bot-input, .inbenta-bot-button.footer__form__button').css('display','none');
     $('#inbenta-bot-input').after('<div class="custom-input-wrapp"><input type="text" class="custom-input" autocomplete="off" placeholder="Custom ask me" id="custom-input"/><button id="custom-send">SEND</button></div>');
+    $(document).on('click', '#custom-send', function(){
+      window.customData = jQuery("#custom-input").val();
+      let messageData = {
+        message: window.customData
+      }
+      if(window.customData && window.customData.length>0){
+        window.chatbot.actions.displayUserMessage(messageData);
+        window.chatbot.actions.sendMessage(messageData);
+        setTimeout(function(){
+          $('#custom-input').val('');
+        }, 500);
+      }
+      
+    });
     // end custom input
   }, 3000);
 
@@ -239,7 +253,7 @@ InbentaChatbotSDK.build(InbentaAuth, {
       }
     }
 });
-  $(document).on('click', '.inbenta-bot-button', function(){
+  $(document).on('click', '.inbenta-bot-button.footer__form__button', function(){
     window.clickNaKnopku = jQuery("#inbenta-bot-input").attr('data-value');
     var messageData = {
       message: window.clickNaKnopku
