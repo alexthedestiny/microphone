4816f17b2a5409dcdf84a912641968ad1f5ea248
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-20 12:29:48 +0300

    insert log in db

--- index.js
+++ index.js
@@ -14,37 +14,47 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database connected");
-  db.close();
-});
-
 app.get('/', function(req, res) {
   res.sendFile('index.html');
   res.sendStatus(200);
 });
 
 app.post('/log', function(req, res){
-	var arr = [];
-	if(!fs.existsSync('./log.json')){
-		fs.writeFile('./log.json', '[{"test":"value"}]', (err) => {
-			console.log("write");
-		});
-	}
-	fs.readFile('./log.json', 'utf8', function(err, contents) {
-	    arr = JSON.parse(contents);
-	    arr.unshift(req.body);
-	    var json = JSON.stringify(arr);
-    	fs.writeFile('./log.json', json, (err) => {
-		    if (err) {
-		        console.error(err);
-		        res.sendStatus(500);
-		        return;
-		    };
-			res.sendStatus(200);
-		});
+	MongoClient.connect(url, function(err, db) {
+	  	if (err) throw err;
+	  	console.log("Database connected");
+	  	var log = db.collection("log");
+	  	var logItem = req.body;
+	  	log.insertOne(logItem, function(err, result){
+	        if(err){ 
+	        	res.sendStatus(500);
+	            return console.log(err);
+	        }
+	        console.log('inserted');
+	        console.log(result.ops);
+	        res.sendStatus(200);
+	        db.close();
+	    });
 	});
+	// var arr = [];
+	// if(!fs.existsSync('./log.json')){
+	// 	fs.writeFile('./log.json', '[{"test":"value"}]', (err) => {
+	// 		console.log("write");
+	// 	});
+	// }
+	// fs.readFile('./log.json', 'utf8', function(err, contents) {
+	//     arr = JSON.parse(contents);
+	//     arr.unshift(req.body);
+	//     var json = JSON.stringify(arr);
+ //    	fs.writeFile('./log.json', json, (err) => {
+	// 	    if (err) {
+	// 	        console.error(err);
+	// 	        res.sendStatus(500);
+	// 	        return;
+	// 	    };
+	// 		res.sendStatus(200);
+	// 	});
+	// });
 });
 
 app.get('/log', function(req, res){
