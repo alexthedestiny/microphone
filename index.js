61227f48ea3502a66cf8f7fa146adbcfadc18562
Unknown <oleksandrpavlenko@yahoo.com>
2018-04-20 12:22:38 +0300

    mongoDB init

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+'use strict';
+
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var fs = require("fs");
+const MongoClient = require('mongodb').MongoClient;
 const PORT = process.env.PORT || 5000;
-
+const url = 'mongodb://admin:admin@ds251819.mlab.com:51819/logdb';
 var app = module.exports = express();
 
 // serve static files
 app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database connected");
+  db.close();
+});
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
